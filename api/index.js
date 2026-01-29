cc93af72b012be1876ca4d1cbcf4c18446889d81
arafats-tech <nahid16192224@gmail.com>
2026-01-29 15:23:23 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,21 +1,2 @@
 import app from '../src/app.js';
-
-// Vercel serverless handler
-export default async (req, res) => {
-  // Add CORS headers if needed
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  
-  // Handle OPTIONS request for CORS
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  
-  // Forward to Express app
-  return app(req, res);
-};
+export default app;
